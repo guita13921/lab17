785141fe3969a0bc69897d10ca06ff5e4d27ae34
guita13921 <guita13921@gmail.com>
2022-01-18 16:06:58 +0700

    lab17_1.cpp compete

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,41 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a+i);
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a,int M,int N){
+    for(int i = 0; i < N*M; i++){
+        a[i] = double(rand()%101)/100;
+    }
+}
+
+void findRowSum(const double *a,double *b,int N,int M){
+    for(int j=0;j<N;j++){
+        double sum = 0;    
+            for(int i = 0;i<M;i++){
+                sum += *((a+i)+(M*j));
+            }
+    b[j] = sum;        
+    }        
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+    int j = 0;
+    double sum = 0.00;
+    while(j < M){
+        sum = 0;
+            for(int i = 0;i < N;i++){
+                sum += *((a + j) + (i*M));
+        }
+        b[j] = sum;
+        j++;
+    }
+}
